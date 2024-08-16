e8584632c2192dee2e2a65c5342d623add273abd
takemuratakemura <yuuki_takemura@mail.toyota.co.jp>
2024-08-16 22:06:32 +0900

    Add files via upload
    new upload

--- ControlProcess.py
+++ ControlProcess.py
@@ -1,13 +1,13 @@
 #駆動/重心制御(プロセス2)記載用pyファイル
 import time
-import socket
-import numpy as np
 from common.State import State
-from common.SharedObject import SharedObject
+
+'''UDP通信用ヘッダ(UDP①)'''
+import socket
+import time
 from struct import pack, unpack
-JOYSTICK = 0
-PSCONTROLLER = 1
-MICROBIT = 2
+'''UDP通信用ヘッダ'''
+
 
 #取得した現stateを表示する関数例
 def printState(state):
@@ -20,48 +20,25 @@ def printState(state):
     else:
         print('p2:Current State is FAIL_SAFE_MODE')
 
+
 #駆動/重心制御用としてプロセス化される関数 ※process2=controlProcessのメイン関数にあたる
 def worker(shared_obj):
     #★★プロセス開始時の初期設定(ローカル変数/定数の設定)や初期化処理を記述
-    '''■■■横軸と縦軸の現在〜過去値'''
-    f4_x     = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #横方向の値 0番目が生値相当 10回平均
-    f4_y     = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #縦方向の値 0番目が生値相当 10回平均
-    f4_speed = [0, 0, 0, 0, 0] #車速 0番目が生値相当 5回平均
-    i4_state = [0, 0] #車両状態 0番目が現在値、1番目が前回値 ※想定外の状態遷移防止
-    '''■■■ここまで'''
-
-    #コントローラ種別
-    i4_ctr_type = shared_obj.i4g_p0_CTRtype.value
-    
-    '''■■■PoC流用'''
-    i = 0
-    dummy= 0
-    p1_time_max = 0
-    Max_Torque = 450.0
-    Min_Torque = -Max_Torque
-    Torque_UpperLimit = Max_Torque
-    Torque_LowerLimit = -Torque_UpperLimit
-    Max_Velo = 200.0 #rps
-    Velo_Tor = 50.0 #最大車速超過許容
-    Pos_Range = 100 #mm
-
-    # 初期接続処理
+    '''UDP通信用初期化処理(UDP②)'''
     M_SIZE = 32
+    dummy = 0
+    Max_Torque = 450.0 #Nm
+    Pos_Range = 100 #mm
     locaddr = ('0.0.0.0', 9998)
-    sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(0.01)
-    '''■■■PoC流用ここまで'''
+    sock.bind(locaddr)
+    communication_counter = 0
 
-    #コントローラの接続はOperationProcessへ
-    sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
-    communication_counter=0
-    
-    ##受信確認
+    #受信確認
     message, cli_addr = sock.recvfrom(M_SIZE)
 
-    #駆動関係の通信初期設定
-    '''■■■PoC流用'''
-    ##モータ状態リセットを送信
+	##モータ状態リセットを送信
     state_command = 0x06 #0x01 servo on, 0x02 stop, 0x04 reset
     send_data = pack('<4sHHHHh', b'TRQT', dummy, communication_counter,  dummy, state_command ,0)
     sock.sendto(send_data,cli_addr)
@@ -72,90 +49,66 @@ def worker(shared_obj):
     state_command = 0x01 #0x01 servo on, 0x02 stop, 0x04 reset
     send_data = pack('<4sHHHHh', b'TRQT', dummy, communication_counter,  dummy, state_command ,0)
     sock.sendto(send_data,cli_addr)
-    '''■■■PoC流用ここまで'''
-
+    
+    '''UDP通信用初期化処理'''
     
     #駆動/重心制御用プロセスのメインループ
     while True:
-        p1_start = time.time()
-        try : 
-            '''■■■グローバル変数からローカル変数へ値更新'''
-            #車両状態取得（[0]今回値、[1]前回値） 0:IG_ON_MODE, 1:PARKING_MODE, 2:DRIVE_MODE, 3:FAIL_SAFE_MODE
-            i4_state = input_raw_data(i4_state, shared_obj.__i4g_p0_state.value)
+        #★★shared_obj定義の共有変数からローカル変数への値読み出し※計算に使用するものなど必要なものを読み出す
+        #【例】"ローカル変数" = shared_obj."共有変数".value
+        state = shared_obj.state #__i4g_p0_stateはSharedObject.py内のstate関数経由で読み出すため「shared_obj.state」の記述で良く「.value」は付けない
 
-            #コントローラからの入力値を取得
-            f4_x = input_raw_data(f4_x, shared_obj.f4g_p1_joyAxisLR.value) #横軸生値更新
-            f4_y = input_raw_data(f4_y, shared_obj.f4g_p1_joyAxisFB.value) #縦軸生値更新
-            f4_speed =  input_raw_data(f4_speed, shared_obj.f4g_p2_speed.value) #車速生値更新
-            u1_Pkb = shared_obj.u1g_p1_Pkb.value #PKB取得
-            f4_x_ave = np.average(f4_x) #配列分だけ単純平均
-            '''■■■ここまで'''
-
-            #★★shared_obj定義の共有変数からローカル変数への値読み出し※計算に使用するものなど必要なものを読み出す
-            #【例】"ローカル変数" = shared_obj."共有変数".value
-            state = shared_obj.state
-
-            #★★計算や処理 ※worker関数外に別関数を定義して呼び出す記載にしても良い
-            printState(state) #【例】
-
-            #【暫定】入力チェック
-            print(f4_x[0], f4_x_ave)
+        '''UDP通信用ループ処理(UDP③)'''
+		#コントローラからの入力（global経由）
+        js_bf_local = shared_obj.f4g_p1_joyAxisFB.value
+        js_lr_local = shared_obj.f4g_p1_joyAxisLR.value
+        js_stp_local = shared_obj.b1g_p0_Stop.value
         
-            '''■■■UDPの通信'''
-
-            ###UDP受信
-            
-            #print("x",val1_loacl[0])
-            # ★★↑
-            # ★★↑
-            # ★★↑
-            #print("process2 -> cycle:{}, value0:{}, value1:{}, value2:{},".format(i, val0_loacl[0], val1_loacl[0], val3_loacl[0]))
-            i = i + 1
-            
-            message, cli_addr = sock.recvfrom(M_SIZE)
-            command, sender_sequence, Act_Velo, Act_Torque, state, mode,dummy_h,dummy_b  = unpack('<6sHlhHbhb', message)  
-            
-            
-            Velo = float(Act_Velo) * 7.5 / 360 #cnt/sec -> rps
-            
-            #Torque_UpperLimit = min(max(0.0,Max_Torque*(1-(Velo-Max_Velo)/Velo_Tor)),Max_Torque)
-            #Torque_LowerLimit = max(min(0.0,Min_Torque*(1+(Velo+Max_Velo)/Velo_Tor)),Min_Torque)
-            if communication_counter < 65535:
-                communication_counter = communication_counter+1
-            else:
-                communication_counter = 0
-                
-            torque = val1_loacl[0]*Max_Torque
-            torque = int(torque)
-            #torque =int(max(min(torque,Torque_UpperLimit),Torque_LowerLimit))
-            
-            state_command = 0x01 #Servo ON
-            #if i % 20 == 0: 
-            print("MorVelo:", Velo, "[rps] Torque_cmd:", torque, "[Nm]  Torque_act:", Act_Torque)
-
-            send_data = pack('<4sHHhHh', b'TRQT', dummy, communication_counter,  pos_lr, state_command,torque)
-            sock.sendto(send_data,cli_addr)
-            '''■■■ここまで'''
-
-            p1_end = time.time()
-            p1_time = p1_end - p1_start
-            p1_time_max = max(p1_time_max, p1_time)
-            #print(p1_time_max)
-            time.sleep(max(0.001,(0.05-p1_time)))
-
-            #★★shared_obj定義の共有変数への書き込み
-            #【例】shared_obj."共有変数".value = "ローカル変数"
-
-
-        except KeyboardInterrupt:
+        torque = int(js_bf_local * Max_Torque)
+        pos = int(js_lr_local * Pos_Range)
+        
+        #コントローラのSTOPボタンが推されたら通信終了
+        if js_stp_local:
             sock.close()
+            time.sleep(1)
             break
-            
-        #★★必要に応じて待ち時間を設定
-        time.sleep(0.01)
+		
+		#受信
+        message, cli_addr = sock.recvfrom(M_SIZE)
+        command, sender_sequence, Act_Velo, Act_Torque, state, mode, dummy_h, dummy_b = unpack('<6sHlhHbhb', message)
+		
+		#Torque_UpperLimit = min(max(0.0,Max_Torque*(1-(Velo-Max_Velo)/Velo_Tor)),Max_Torque)
+        #Torque_LowerLimit = max(min(0.0,Min_Torque*(1+(Velo+Max_Velo)/Velo_Tor)),Min_Torque)
+        if communication_counter < 65535:
+            communication_counter = communication_counter+1
+        else:
+            communication_counter = 0
 
-#0番目に生値を格納して過去値を1つずらす
-def input_raw_data(arr, raw):
-    new_arr = np.roll(arr, 1)
-    new_arr[0] = raw
-    return new_arr
+        Velo = float(Act_Velo) * 7.5 / 360 #cnt/sec -> rps
+
+        state_command = 0x01 #Servo ON
+        #if i % 20 == 0: 
+        print("MorVelo:", Velo, "[rps] Torque_cmd:", torque, "[Nm]  Torque_act:", Act_Torque)
+
+
+		#送信
+        state_command = 0x01
+        send_data = pack('<4sHHhHh', b'TRQT', 0, communication_counter, pos, state_command, torque)
+        sock.sendto(send_data, cli_addr)
+		
+        '''UDP通信用ループ処理'''
+
+
+        #★★計算や処理 ※worker関数外に別関数を定義して呼び出す記載にしても良い
+        printState(state) #【例】
+        
+        #★★shared_obj定義の共有変数への書き込み
+        #【例】shared_obj."共有変数".value = "ローカル変数"
+
+        #★★必要に応じて待ち時間を設定
+        #time.sleep(1)
+        p1_end = time.time()
+        p1_time = p1_end - p1_start
+        p1_time_max = max(p1_time_max, p1_time)
+        #print(p1_time_max)
+        time.sleep(max(0.001,(0.05-p1_time)))
--- OperationProcess.py
+++ OperationProcess.py
@@ -1,53 +1,8 @@
 #操作/表示系(プロセス1)記載用pyファイル
 import time
 from common.State import State
-#from bluepy.btle import DefaultDelegate, Peripheral,ADDR_TYPE_RANDOM
-from common.SharedObject import SharedObject
-
-#コントローラ入力取得
+'''joystick用初期化処理(joystick①)'''
 import pygame
-import socket
-
-#コントローラ種別
-JOYSTICK = 0
-PSCONTROLLER = 1
-MICROBIT = 2
-
-'''■■■PoC流用 ここはControlProcessとどっちがいいか確認'''
-i = 0
-dummy= 0
-p1_time_max = 0
-Max_Torque = 450.0
-Min_Torque = -Max_Torque
-Torque_UpperLimit = Max_Torque
-Torque_LowerLimit = -Torque_UpperLimit
-Max_Velo = 200.0 #rps
-Velo_Tor = 50.0 #最大車速超過許容
-Pos_Range = 100 #mm
-
-'''■■■PoC流用ここまで'''
-
-
-'''Microbit初期設定'''
-# MicrobitのMAC
-MAC_ADDRESS = 'F3:52:BC:B2:86:72'
-
-#ACCELEROMETER SERVICE/CHARACTERISTICS UUID
-ACC_SERVICE_UUID = 'E95D0753251D470AA062FA1922DFA9A8'
-ACC_CHARACTERISTICS_UUID = 'E95DCA4B251D470AA062FA1922DFA9A8'
-
-#BUTTON SERVICE/BUTTON A CHARACTERISTICS UUID
-BTN_SERVICE_UUID = 'E95D9882251D470AA062FA1922DFA9A8'
-BTN_A_CHARACTERISTICS_UUID = 'E95DDA90251D470AA062FA1922DFA9A8'
-BTN_B_CHARACTERISTICS_UUID = 'E95DDA91251D470AA062FA1922DFA9A8'
-
-#TEMPATURE SERVICE/CHARACTERISTICS UUID
-TMP_SERVICE_UUID = 'E95D6100251D470AA062FA1922DFA9A8'
-TMP_CHARACTERISTICS_UUID = 'E95D9250251D470AA062FA1922DFA9A8'
-'''Microbit初期設定ここまで'''
-
-#SharedObjectのインスタンスを定義
-so = SharedObject()
 
 #取得した現stateを表示する関数例
 def printState(state):
@@ -64,96 +19,51 @@ def printState(state):
 #操作/表示系用としてプロセス化される関数 ※process1=operationProcessのメイン関数にあたる
 def worker(shared_obj):
     #★★プロセス開始時の初期設定(ローカル変数/定数の設定)や初期化処理を記述
+    STOP_FLAG = False #停止フラグ
+    p0_time_max = 0   #定期周期用タイマ
+    js_input_lr = 0 #左右ジョイスティックの値初期化
+    js_input_bf = 0 #前後ジョイスティックの値初期化
 
-    #コントローラ種別
-    i4_ctr_type = shared_obj.i4g_p0_CTRtype.value
+    '''joystick用初期化処理(joystick②)'''
+    pygame.init()
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    '''joystick用初期化処理'''
 
-    # 初期接続処理
-    match i4_ctr_type:
-#        case 0: #JOYSTICK:
-        case 1: #PSCONTROLLER:
-            '''PSコントローラの接続前設定'''
-            M_SIZE = 32
-            locaddr = ('0.0.0.0', 9998)
-            sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
-            sock.settimeout(0.01)
-            print('create socket')
-
-            sock.bind(locaddr)
-
-            ##受信確認
-            message, cli_addr = sock.recvfrom(M_SIZE)
-
-        case 2: #MICROBIT:
-            # 接続設定
-            peripheral = Peripheral(MAC_ADDRESS, ADDR_TYPE_RANDOM)
-            # - 加速度センサー
-            acc_service = peripheral.getServiceByUUID(ACC_SERVICE_UUID)
-            acc_characteristic = peripheral.getCharacteristics(uuid=ACC_CHARACTERISTICS_UUID)
-            # - ボタン
-            btn_service = peripheral.getServiceByUUID(BTN_SERVICE_UUID)
-            btn_A_characteristic = peripheral.getCharacteristics(uuid=BTN_A_CHARACTERISTICS_UUID)
-            btn_B_characteristic = peripheral.getCharacteristics(uuid=BTN_B_CHARACTERISTICS_UUID)
-            # - 温度センサー
-            tmp_service = peripheral.getServiceByUUID(TMP_SERVICE_UUID)
-            tmp_characteristic = peripheral.getCharacteristics(uuid=TMP_CHARACTERISTICS_UUID)
-        case _:
-            print("invalid controller 1")
-    
+ 
     #操作/表示系用プロセスのメインループ
     while True:
         #★★shared_obj定義の共有変数からローカル変数への値読み出し※計算に使用するものなど必要なものを読み出す
-        #コントローラからデータを取得
-        match i4_ctr_type:
-#            case 0: #JOYSTICK:
-            case 1: #PSCONTROLLER:
-                STOP_FLAG = shared_obj.b1g_p0_Stop.value
-                if STOP_FLAG == True:
-                    sock.close()
-                    time.sleep(1)
-                    break
-                    
-                #★★★かとしょーさんに確認。もともとのval1_local[#]が何だったか？
-                #trq_bf = val1_loacl[0]*Max_Torque
-                #pos_lr = int(val1_loacl[1]*Pos_Range)
-                trq_bf = shared_obj.f4g_p1_joyAxisFB.value * Max_Torque
-                pos_lr = int(shared_obj.f4g_p1_joyAxisLR.value * Pos_Range)
-
-
-            case 2: #MICROBIT:
-                '''Microbitデータ読み出し'''
-                acc_read_data  = acc_characteristic[0].read()
-                btnA_read_data = btn_A_characteristic[0].read()
-                btnB_read_data = btn_B_characteristic[0].read()
-                tmp_read_data  = tmp_characteristic[0].read()
-
-                # 加速度
-                axis_x = int.from_bytes(acc_read_data[0:2], byteorder='little', signed=True)
-                axis_y = int.from_bytes(acc_read_data[2:4], byteorder='little', signed=True)
-                axis_z = int.from_bytes(acc_read_data[4:6], byteorder='little', signed=True)
-                '''Microbit読み出しここまで'''
-            case _:
-                print("invalid controller 2")
-            
         #【例】"ローカル変数" = shared_obj."共有変数".value
-        state = shared_obj.state
+        state = shared_obj.state #__i4g_p0_stateはSharedObject.py内のstate関数経由で読み出すため「shared_obj.state」の記述で良く「.value」は付けない
+        
+        '''joystick用ループ処理（joystick③)'''
+        if pygame.event.get():
+            #ジョイスティックの操作を読み取る
+            js_input_lr = joystick.get_axis(0) #左のジョイスティックの左右方向
+            #print(js_input_lr)
+            js_input_bf = joystick.get_axis(4) #右のジョイスティックの上下方向
+            #print(js_input_bf)
+
+            # Aボタンが押されたら終了
+            if joystick.get_button(0):
+                print('stop!')
+                STOP_FLAG = True
+                break
+
+		#共用変数へ書き込み
+        shared_obj.f4g_p1_joyAxisFB.value = js_input_bf
+        shared_obj.f4g_p1_joyAxisLR.value = js_input_lr
+        shared_obj.b1g_p0_Stop.value = STOP_FLAG 
+        '''joystick用ループ処理'''
+
 
         #★★計算や処理 ※worker関数外に別関数を定義して呼び出す記載にしても良い
         printState(state) #【例】
         
         #★★shared_obj定義の共有変数への書き込み
         #【例】shared_obj."共有変数".value = "ローカル変数"
-        match i4_ctr_type:
-#            case 0: #JOYSTICK:
-#            case 1: #PSCONTROLLER:
-            case 2: #MICROBIT:
-                shared_obj.f4g_p1_joyAxisLR.value = axis_x #横方向をグローバルに書き込み
-                shared_obj.f4g_p1_joyAxisFB.value = axis_y #縦方向をグローバルに書き込み
-                shared_obj.u1g_p1_Pkb.value = btnB_read_data #PKB状態を書き込み
-
-#            case _:
-#                print("invalid controller 3")
 
 
         #★★必要に応じて待ち時間を設定
-        time.sleep(1)
+        time.sleep(0.1)
